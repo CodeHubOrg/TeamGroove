feca30622be7f1b55191e0c17307544dd4642dce
stiffneckjim <22213990+stiffneckjim@users.noreply.github.com>
2021-02-25 23:37:38 +0000

    Reformatted wsgi.py with black

--- team_groove/wsgi.py
+++ team_groove/wsgi.py
@@ -12,12 +12,11 @@ import dotenv
 
 from django.core.wsgi import get_wsgi_application
 
-dotenv.load_dotenv(
-    os.path.join(os.path.dirname(os.path.dirname(__file__)), '.env')
-)
+dotenv.load_dotenv(os.path.join(os.path.dirname(os.path.dirname(__file__)), ".env"))
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'team_groove.settings.development')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "team_groove.settings.development")
 
-if os.getenv('DJANGO_SETTINGS_MODULE'): os.environ['DJANGO_SETTINGS_MODULE'] = os.getenv('DJANGO_SETTINGS_MODULE')
+if os.getenv("DJANGO_SETTINGS_MODULE"):
+    os.environ["DJANGO_SETTINGS_MODULE"] = os.getenv("DJANGO_SETTINGS_MODULE")
 
 application = get_wsgi_application()
