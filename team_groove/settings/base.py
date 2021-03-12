702f39a7735019e16b4e5e1724a679941a6902d8
AsquaredWsquared <76879959+AsquaredWsquared@users.noreply.github.com>
2021-03-12 19:23:37 +0000

    Skeleton frontpage for teamgroove

--- team_groove/settings/base.py
+++ team_groove/settings/base.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+
+    "core",
 ]
 
 MIDDLEWARE = [
@@ -55,7 +57,7 @@ ROOT_URLCONF = "team_groove.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [ BASE_DIR / "templates" ],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
