be9e40027b7891661c81d7c49e676a0459eb7de7
AsquaredWsquared <76879959+AsquaredWsquared@users.noreply.github.com>
2021-03-13 15:55:15 +0000

    Revert "Front page 0.1"
    This reverts commit e1b8d8cca53ab2c296c37bebe600588c041ac5c9.
    
    tidying up the git mess

--- core/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render
-from django.views.generic import TemplateView
-
-class FrontPageView(TemplateView):
-    template_name = "frontpage.html"
--- team_groove/settings/base.py
+++ team_groove/settings/base.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = "team_groove.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [ BASE_DIR / "templates" ],
+        "DIRS": [],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
