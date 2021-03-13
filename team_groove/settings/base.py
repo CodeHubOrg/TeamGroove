bd07617d990dc6f6d51460c748820efdb97dac89
AsquaredWsquared <76879959+AsquaredWsquared@users.noreply.github.com>
2021-03-13 19:31:00 +0000

    Password validator

--- team_groove/settings/base.py
+++ team_groove/settings/base.py
@@ -102,6 +102,11 @@ AUTH_PASSWORD_VALIDATORS = [
     {
         "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
+    {
+        'NAME': 'users.validators.MaximumLengthValidator',
+            'OPTIONS': {
+                'max_length': 200, }
+    },
 ]
 
 
