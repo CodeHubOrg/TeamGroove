c7576db61b79c76cae77008cf246cdfde6df5c5f
AsquaredWsquared <76879959+AsquaredWsquared@users.noreply.github.com>
2021-03-13 19:06:53 +0000

    Custom User email as userid

--- team_groove/settings/base.py
+++ team_groove/settings/base.py
@@ -28,6 +28,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+AUTH_USER_MODEL = 'users.CustomUser'
 
 # Application definition
 
@@ -38,6 +39,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+
+    "core",
+    "users",
 ]
 
 MIDDLEWARE = [
