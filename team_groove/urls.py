e1b8d8cca53ab2c296c37bebe600588c041ac5c9
AsquaredWsquared <76879959+AsquaredWsquared@users.noreply.github.com>
2021-03-13 10:59:31 +0000

    Front page 0.1

--- team_groove/urls.py
+++ team_groove/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from core.views import FrontPageView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', FrontPageView.as_view(), name='frontpage'),
 ]
