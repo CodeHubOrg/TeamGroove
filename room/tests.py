d85ad1156944fe465db837ed5c74dc6be48ddc5e
AsquaredWsquared <76879959+AsquaredWsquared@users.noreply.github.com>
2021-04-23 19:32:38 +0100

    Tests for the room forms

--- room/tests.py
+++ room/tests.py
@@ -1,3 +1,57 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .forms import AddRoom, AcceptInvitation, EmailInvite, EditRoom
+
+class AddRoomFormTests(TestCase):
+
+    def test_title_is_less_than_255(self):
+        form = AddRoom(data={"title": "a test room name"})
+
+        self.assertEqual(form.errors, {})
+
+    def test_title_is_greater_than_255(self):
+        form = AddRoom(data={"title": "test" * 64 })
+
+        self.assertEqual(form.errors['title'], ['Ensure this value has at most 255 characters (it has 256).'])
+
+class AcceptInvitationFormTests(TestCase):
+
+    def test_invitation_code_is_less_than_20(self):
+        form = AcceptInvitation(data={"invitation_code": "1234567890"})
+
+        self.assertEqual(form.errors, {})
+    
+    def test_invitation_code_is_greater_than_20(self):
+        form = AcceptInvitation(data={"invitation_code": "a" * 21})
+
+        self.assertEqual(form.errors['invitation_code'], ['Ensure this value has at most 20 characters (it has 21).'])
+
+class EmailInviteFormTests(TestCase):
+
+    def test_email_invite_is_email(self):
+        form = EmailInvite(data={"email": "test@test"})
+
+        self.assertEqual(form.errors['email'], ['Enter a valid email address.'])
+
+    def test_email_invite_is_less_than_255(self):
+        form = EmailInvite(data={"email": "test@example.com"})
+
+        self.assertEqual(form.errors, {})
+
+    def test_email_invite_is_less_than_255(self):
+        form = EmailInvite(data={"email": "test" * 64 + "@example.com"})
+
+        self.assertEqual(form.errors['email'], ['Ensure this value has at most 255 characters (it has 268).'])
+
+class EditRoomFormTests(TestCase):
+
+    def test_title_is_less_than_255(self):
+        form = EditRoom(data={"title": "a test room name"})
+
+        self.assertEqual(form.errors, {})
+
+    def test_title_is_greater_than_255(self):
+        form = EditRoom(data={"title": "test" * 64 })
+
+        self.assertEqual(form.errors['title'], ['Ensure this value has at most 255 characters (it has 256).'])
+
