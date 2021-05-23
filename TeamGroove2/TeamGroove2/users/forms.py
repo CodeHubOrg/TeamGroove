5c5499ad338191814fa2e3de460a8ebe4e6407f7
Adam Ward <ilikebaconrollsandcoffee@gmail.com>
2021-05-23 20:46:11 +0100

    Fixed error in tests

--- TeamGroove2/TeamGroove2/users/forms.py
+++ TeamGroove2/TeamGroove2/users/forms.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 
-from .models import CustomUser
+from users.models import CustomUser
 
 
 class CustomUserCreationForm(UserCreationForm):
---
+++ TeamGroove2/config/settings/test.py
@@ -0,0 +1,33 @@
+"""
+With these settings, tests run faster.
+"""
+
+from .base import *  # noqa
+from .base import env
+
+# GENERAL
+# ------------------------------------------------------------------------------
+# https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
+SECRET_KEY = env(
+    "DJANGO_SECRET_KEY",
+    default="h3ZCYTyide6Tbf1JzXHUAgX0UleB9oO0xu83nWpomP6C7LUKJmhDyUKo8HMilp4s",
+)
+# https://docs.djangoproject.com/en/dev/ref/settings/#test-runner
+TEST_RUNNER = "django.test.runner.DiscoverRunner"
+
+# PASSWORDS
+# ------------------------------------------------------------------------------
+# https://docs.djangoproject.com/en/dev/ref/settings/#password-hashers
+PASSWORD_HASHERS = ["django.contrib.auth.hashers.MD5PasswordHasher"]
+
+# TEMPLATES
+# ------------------------------------------------------------------------------
+TEMPLATES[-1]["OPTIONS"]["loaders"] = [  # type: ignore[index] # noqa F405
+    (
+        "django.template.loaders.cached.Loader",
+        [
+            "django.template.loaders.filesystem.Loader",
+            "django.template.loaders.app_directories.Loader",
+        ],
+    )
+]
