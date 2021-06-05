f8b89544f5d99c805f15104d414f50e6fa15ceff
Kwan Yee CHAN <59257733+kyc-wip@users.noreply.github.com>
2021-06-05 18:53:09 +0100

    [#19 #28] Add draft .py file content to spotify/views.py
    Add draft add_track_id_to_playlist.py and search_track_name_on_spotify.py file content to spotify/views.py.
    Delete draft .py files and associated .html files.
    
    Requires integration, test and refactoring
    In preparation:
    - Print statements converted to messages
    - Add url to handle_search_results.html to main urls.py
    - Add handle_search_results.html file to TeamGroove
    - Add example test parameters (commented) to spotify/test.py

--- spotify/views.py
+++ spotify/views.py
@@ -1,8 +1,14 @@
+from typing import NoReturn
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 
 import spotipy
 import os
+
+
+# Default parameter value to start while loop
+NEXT = 'https://api.spotify.com'
 
 
 caches_folder = './.spotify_caches/'
@@ -38,3 +44,173 @@ def authorize_with_spotify(request, spotify_code=None):
     }
 
     return render(request, 'user_playlists.html', context)
+
+# TO DO: Refactor functions
+@login_required
+def get_playlist_items(request, auth_manager, PLAYLIST_ID):
+    spotify = spotipy.Spotify(auth_manager=auth_manager)
+
+    MAX_ITEMS_LIMIT    = 100
+    OFFSET             = 0
+    OFFSET_UPPER_LIMIT = 10
+    playlist_items     = None
+
+    while ( ( NEXT != 'null' | NEXT != None ) & ( OFFSET <= OFFSET_UPPER_LIMIT ) ):
+        try:
+            returned_data = spotify.playlist_items(playlist_id=PLAYLIST_ID, 
+                                                   fields=None, 
+                                                   limit=MAX_ITEMS_LIMIT, 
+                                                   offset=OFFSET, 
+                                                   market='GB', 
+                                                   additional_types='track')
+            LIMIT = returned_data['limit']
+            NEXT  = returned_data['next']
+            TOTAL = returned_data['total']
+
+            if TOTAL > 0:
+                playlist_items = append_track_data(returned_data, LIMIT)
+            else:
+                break
+        except:        
+            messages.error(request, 'ERROR ' + request['error']['status'] + ': ' + 'Playlist Items ' + request['error']['message'])
+        else:
+            messages.success(request, 'SUCCESS: Playlist Items Fetched')
+        
+        OFFSET += 1
+
+        if OFFSET > OFFSET_UPPER_LIMIT:
+            messages.error(request, 'ERROR: There are over 1000 songs in your playlist? Let''s stop here!')
+            break
+        else:
+            pass
+    
+    return playlist_items
+
+# Scenario 1: Add a new track using a valid Spotify track name - Happy Path
+# Scenario 2a: Search for a new track using an ambiguous Spotify track name - Happy Path
+# Scenario 2b: Search for a new track to add to the playlist - Happy Path
+@login_required
+def search_track_name_on_spotify(request, auth_manager, QUERY, PLAYLIST_ID):
+    spotify = spotipy.Spotify(auth_manager=auth_manager)
+
+    MAX_SEARCH_LIMIT   = 100
+    OFFSET             = 0
+    OFFSET_UPPER_LIMIT = 1
+    search_results     = None
+
+    while ( ( NEXT != 'null' | NEXT != None ) & ( OFFSET <= OFFSET_UPPER_LIMIT ) ):
+        try:
+            returned_data = spotify.search(q=QUERY, limit=MAX_SEARCH_LIMIT, offset=OFFSET, type='track', market='GB')
+
+            LIMIT = returned_data['limit']
+            NEXT  = returned_data['next']
+            TOTAL = returned_data['total']
+            
+            if TOTAL > 0:
+                search_results = append_track_data(request, returned_data, LIMIT)
+            else:
+                messages.info(request, 'INFO: No tracks found.')
+                break
+        except:
+            messages.error(request, 'ERROR: Unknown error fetching track names.')
+
+        OFFSET += 1
+
+        if OFFSET > OFFSET_UPPER_LIMIT:
+            messages.error(request, 'ERROR: There are over 100 search results! Let''s stop here!')
+            break
+        else:
+            pass
+
+    return handle_search_results(search_results, PLAYLIST_ID)
+
+@login_required
+def handle_search_results(request, search_results, PLAYLIST_ID):
+    if len(search_results['track_id']) == 1:
+        # Scenario 1: Add a new track using a valid Spotify track name - Happy Path
+        TRACK_ID = search_results['track_id']
+        add_track_id_to_playlist(request, PLAYLIST_ID, TRACK_ID) 
+    elif len(search_results['track_id']) > 1:
+        # Scenario 2a: Search for a new track using an ambiguous Spotify track name - Happy Path
+        # Scenario 2b: Search for a new track to add to the playlist - Happy Path
+        context = search_results
+        return render(request, 'handle_search_results.html', context)  
+    else:
+        messages.info(request, 'INFO: No track available.')
+        return redirect('room.html')  
+    # Scenario 3: Add a new track using an invalid Spotify track name - Unhappy Path
+    # Handled by add_track_id_to_playlist
+    # Scenario 4: Add a duplicate track using a Spotify ID - Unhappy Path
+    # Handled by add_track_id_to_playlist
+
+@login_required
+def add_track_id_to_playlist(request, auth_manager, PLAYLIST_ID, TRACK_ID):
+    spotify = spotipy.Spotify(auth_manager=auth_manager)
+
+    # TO DO: Do we need to do this again? 
+    # Should pass parameter to check how we arrived here, to check when playlist_track_ids were last fetched
+    playlist_items = get_playlist_items(PLAYLIST_ID)
+    playlist_track_ids = playlist_items['track_id']
+    new_playlist_track_ids = playlist_track_ids
+
+    if TRACK_ID not in playlist_track_ids:
+        try: 
+            # Scenario 1: Add a new track using a valid Spotify ID - Happy Path
+            # {
+            #         "snapshot_id": "MzgsNjM1NDNkN2I0MGNlNzBjNDc0NGYxODAyOWVhZmEyNjFlZWQxYTZiZg=="
+            # }
+            snapshot_id = spotify.playlist_add_items(playlist_id=PLAYLIST_ID, items=TRACK_ID, position=None)
+        except:
+            # Scenario 2: Add a new track using an invalid Spotify ID - Unhappy Path
+            # {
+            #     "error": {
+            #     "status": 400,
+            #     "message": "Payload contains a non-existing ID"
+            # }
+            messages.error(request, 'ERROR ' + request['error']['status'] + ': ' + request['error']['message'])
+        else:
+            new_playlist_track_ids = get_playlist_items(PLAYLIST_ID)
+            messages.success(request, 'SUCCESS? Add track id to playlist attempted.')
+    else:
+        # Scenario 3: Add a duplicate track using a Spotify ID - Unhappy Path
+        messages.error(request, 'ERROR: Track already on playlist')
+        return redirect('room.html') 
+
+    context = {
+        'PLAYLIST_ID': PLAYLIST_ID,
+        'playlist_track_ids': new_playlist_track_ids
+    }
+
+    return render(request, 'room.html', context)
+
+@login_required
+def append_track_data(request, returned_data, LIMIT):
+    # TRACK_ARTIST, TRACK_NAME and TRACK_ALBUM are intended for view
+    # TRACK_ID is used for adding to a playlist        
+    track_artist = []
+    track_name   = []
+    track_id     = []
+    track_album  = []
+
+    index = 0        
+    while index < LIMIT:
+        try:
+            track_artist.append(returned_data['items'][index]['track']['artists']['name'])
+            track_name.append(returned_data['items'][index]['track']['name']) 
+            track_id.append(returned_data['items'][index]['track']['id'])
+            track_album.append(returned_data['items'][index]['track']['album']['name'])
+            index += 1  
+        except IndexError:
+            break
+        except:
+            messages.error(request, 'ERROR: Unknown Error compiling track ids, artist names and track names.')
+        else:
+            context = {
+                    'track_artist': track_artist,
+                    'track_name':   track_name,
+                    'track_id':     track_id,
+                    'track_album':  track_album
+            }
+    
+    return context
+    
--- team_groove/urls.py
+++ team_groove/urls.py
@@ -22,7 +22,7 @@ from users.views import signup
 from grooveboard.views import grooveboard
 from room.views import add_room, activate_room, room, invite, accept_invitation, edit_room, delete_room
 
-from spotify.views import authorize_with_spotify
+from spotify.views import authorize_with_spotify, handle_search_results
 
 
 from spotify.views import add_track_id_to_playlist # search_track_name_on_spotify
@@ -40,11 +40,10 @@ urlpatterns = [
     path('room/delete_room/<int:room_id>/', delete_room, name='delete_room'),
     path('room/accept_invitation/', accept_invitation, name='accept_invitation'),
 
-    path('spotify/add_track_id_to_playlist/<str:PLAYLIST_ID>', add_track_id_to_playlist, name='add_track_id_to_playlist'),
-#   path('spotify/search_track_name_on_spotify/', search_track_name_on_spotify, name='search_track_name_on_spotify'),
- 
     path('spotify/authorize_with_spotify/<spotify_code>', authorize_with_spotify, name='authorize_with_spotify'),
     path('spotify/authorize_with_spotify/', authorize_with_spotify, name='authorize_with_spotify'),
+    
+    path('spotify/handle_search_results/', handle_search_results, name='handle_search_results'),
        
     path('signup/', signup, name='signup'),
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
