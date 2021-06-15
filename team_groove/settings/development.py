e17642adfbd4f13ab5314187b3af513a022ce2c2
stiffneckjim <22213990+stiffneckjim@users.noreply.github.com>
2021-06-15 22:36:01 +0100

    Move loading of .env file from base.py to development.py and production.py. Set default for loading environment variables from .env file to true in development.py and false in production.py.

--- team_groove/settings/development.py
+++ team_groove/settings/development.py
@@ -1,13 +1,20 @@
 """ Set up environment variables for Team Groove applications specific to production environment"""
 import os
+import environ
 
 from .base import *
-from .base import env
 
 # GENERAL
 # ------------------------------------------------------------------------------
 # https://docs.djangoproject.com/en/dev/ref/settings/#debug
 DEBUG = True
+
+env = environ.Env()
+
+READ_DOT_ENV_FILE = env.bool("DJANGO_READ_DOT_ENV_FILE", default=True)
+if READ_DOT_ENV_FILE:
+    # OS environment variables take precedence over variables from .env
+    env.read_env(str(ROOT_DIR / ".env"))
 
 # https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
 SECRET_KEY = env(
