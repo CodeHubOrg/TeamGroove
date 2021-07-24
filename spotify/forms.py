882967ffbcfd304b72e63bac84ab21381849197e
Kwan Yee CHAN <59257733+kyc-wip@users.noreply.github.com>
2021-07-24 11:29:13 +0100

    Disable and hide playlist_id on search track form

--- spotify/forms.py
+++ spotify/forms.py
@@ -1,5 +1,5 @@
 from django import forms
 
 class SearchTrackName(forms.Form):
-    query = forms.CharField(label='Search by Track Name', max_length=255, required=False)
-    playlist_id = forms.CharField(label='Playlist ID', max_length=255, required=False)
+    query = forms.CharField(label='Search by Track Name', max_length=255, required=True)
+    playlist_id = forms.CharField(label='Playlist ID', max_length=255, required=True)
